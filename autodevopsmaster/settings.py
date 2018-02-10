c33a86d3bb8d999ca71d09dab237759c1b911212
FanFengping <263869564@qq.com>
2018-02-10 17:31:04 +0800

    V1.1.0 提交数据库变更，改为MySQL数据库 by 范丰平

--- autodevopsmaster/settings.py
+++ autodevopsmaster/settings.py
@@ -80,10 +80,22 @@ WSGI_APPLICATION = 'autodevopsmaster.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+# 变更数据库为 MySQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'autodevopsmaster',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'autodevopsmaster',
+        'PASSWORD': 'autodevopsmaster123',
     }
 }
 
